13dab7f07845e50ab5ab3d2bad28c0a874909fd2
hyunbin63 <gusqls060303@naver.com>
2023-03-22 03:17:55 +0900

    Create Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,13 @@
 import './layout.css'
 
+import Header from '@/components/Header/page'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head />
       <body>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/Header/page.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+import style from './style.module.css'
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        
+      </header>
+    </>
+  )
+}
