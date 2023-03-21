41b938fe94837113e8d049866f129c9e99618092
hyunbin63 <gusqls060303@naver.com>
2023-03-22 03:58:18 +0900

    Update Header

--- src/components/Header/page.js
+++ src/components/Header/page.js
@@ -1,12 +1,16 @@
 import './style.css'
 
+import Link from 'next/link'
+
 import style from './style.module.css'
 
 export default function Header() {
   return (
     <>
       <header>
-        
+        <div className={style.headerContent}>
+          <Link className={`${style.title} ${style.noneDeco}`} href="/">HTML</Link>
+        </div>
       </header>
     </>
   )
