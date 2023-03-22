8fd31dcdb489715f5f8733791f49ab5797a070d1
hyunbin63 <gusqls060303@naver.com>
2023-03-22 23:16:14 +0900

    Update Sidebar Content

--- src/components/Sidebar/page.js
+++ src/components/Sidebar/page.js
@@ -7,7 +7,14 @@ export default function Sidebar() {
     <>
       <div className={style.sidebar}>
         <ul>
-          <li><Link href="/">HTML 개요</Link></li>
+          <li><Link href="/">HTML 기초</Link></li>
+          <li><Link href="/">HTML 기본 구조</Link></li>
+          <li><Link href="/">HTML 요소 구조</Link></li>
+          <li><Link href="/">HTML 텍스트 요소</Link></li>
+          <li><Link href="/">HTML 기본 요소</Link></li>
+          <li><Link href="/">HTML 리소스 요소</Link></li>
+          <li><Link href="/">HTML 입력 요소</Link></li>
+          <li><Link href="/">HTML 확장</Link></li>
         </ul>
       </div>
     </>
