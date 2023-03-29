ef42def77664d92372afe3856c99d5c225029b09
hyunbin63 <gusqls060303@naver.com>
2023-03-29 20:39:57 +0900

    Change Code Structure

--- src/app/textTag/page.js
+++ src/app/textTag/page.js
@@ -1,5 +1,7 @@
 import style from './style.module.css'
 
+import P from '@/components/tag/p'
+
 export default function() {
   return (
     <>
@@ -7,19 +9,7 @@ export default function() {
         <h1>HTML 텍스트 태그</h1>
         <hr />
         <div className={style.text}>
-          <h3>p 태그</h3>
-          <p>
-            &lt;p&gt; 태그는 문단을 정의하는 태그입니다 <br />
-            내용 안에서 줄바꿈은 자동으로 적용되지 않으며, &lt;br&gt; 태그를 사용해야 합니다 <br />
-            또한 브라우저는 자동으로 문단의 앞뒤에 여백을 추가합니다 <br />
-            여백은 css를 통해 제어할 수 있습니다 <br />
-          </p>
-          <fieldset>
-            <legend>예시</legend>
-            &lt;p&gt;문단&lt;/p&gt; <br />
-            &lt;p&gt;문단&lt;/p&gt; <br />
-            &lt;p&gt;문단&lt;/p&gt; <br />
-          </fieldset>
+          <P />
         </div>
       </div>
     </>
